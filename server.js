5d95479969e46a624d2258db681f6e9e4e7d89bd
BransonGitomeh <sirbranson@outlook.com>
2016-10-12 17:55:56 +0300

    added little monitoring bot and for notifications

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const express_handlebars = require("express-handlebars")
 const async = require("async")
 const assert = require("assert")
 
-
-console.log("let the games begin")
-
-const async = require("async")
-const assert = require("assert")
 const cassandra = require('cassandra-driver');
 
 // openshift or localhost
@@ -26,5 +21,31 @@ var client = new cassandra.Client(connectionOptions);
 client.connect((err) => {
 	assert.ifError(err)
 	// creating the application code
-	
+	if(!err) bot.sendMessage("21649399", "conencted to cassandra on: " + connectionOptions.contactPoints).then(log)
 })
+
+
+function log(argument) {
+	// console.log(argument)
+}
+
+
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '290896795:AAGjxqsPiaMiAcgqnGoKtMuEqP6I-y2kcR0';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+// Matches /echo [whatever]
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  var fromId = msg.from.id;
+  var resp = match[1];
+  console.log(fromId)
+  bot.sendMessage(fromId, resp);
+});
+
+var os = require("os")
+
+const startMessage  = `server started on: \n` + os.hostname() + " \n" + os.type() + " \n" +  os.platform() + " "
+
+bot.sendMessage("21649399", startMessage).then(log)
